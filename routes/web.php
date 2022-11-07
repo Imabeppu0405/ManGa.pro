940d27179ce6a9aaeca8d8f1be070a63a72d25fa
今別府大輝 <dimabeppu@DESKTOP-49AIDSM.localdomain>
2022-11-07 20:49:44 +0900

    homeの重複排除

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+Route::view('/', 'welcome');
 
 Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
 
